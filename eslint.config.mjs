3dcb003ab814dd51fd97d3fb25deacd34a6c58f9
franpfeiffer <franciscopfeiffer.business@gmail.com>
2025-10-16 15:00:19 -0300

    fix: problems with the test on build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,10 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "**/__test__/**",
+      "**/*.test.ts",
+      "**/*.test.tsx",
+      "test-setup.ts",
     ],
   },
 ];
